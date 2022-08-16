bc08c9c29046767fe31a87a13ca84dc8c252792b
Ulan Duisek <33026225+udrepo@users.noreply.github.com>
2022-08-16 11:59:00 +0600

    Update by_point.js

--- by_point.js
+++ by_point.js
@@ -6,7 +6,7 @@ console.log(urlParams.get('lat'))
 const point = [urlParams.get('lat'), urlParams.get('lon')];
 const balloon = {
   title: urlParams.get('title'),
-  //address: urlParams.get('address')
+  address: urlParams.get('address')
 }
 
 ymaps.ready(init);
@@ -20,7 +20,7 @@ function init() {
 
   let placemark = new ymaps.Placemark(point, {
     balloonContentHeader: balloon.title,
-
+    balloonContentBody: balloon.address
   }, {
     iconLayout: 'default#image',
     iconImageHref: './point.svg',
